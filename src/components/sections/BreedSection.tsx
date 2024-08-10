c8201d569f3b85d1a28e763107a2a2e42d89a4b3
olha-dovbush <olha.dovbush@jito.dev>
2024-08-10 19:31:15 +0300

    feat: use Card component in BreedSection

--- src/components/sections/BreedSection.tsx
+++ src/components/sections/BreedSection.tsx
@@ -1,27 +1,34 @@
 import Image from 'next/image';
 
 import { getBreeds } from '@/api/breeds';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '@/components/ui/card';
 
 export async function BreedSection() {
   const breeds = await getBreeds();
 
   return (
-    <div>
-      <h1>Breeds</h1>
-      <ul>
+    <div className="container">
+      <div className="grid grid-cols-3 gap-4">
         {breeds.map((breed) => (
-          <li key={breed.id}>
-            {breed.name}
-            <Image
-              src={breed.image}
-              alt={breed.name}
-              width={200}
-              height={200}
-              className="h-[200px] w-auto object-cover"
-            />
-          </li>
+          <Card
+            key={breed.id}
+            className="cursor-pointer"
+          >
+            <CardContent className="pb-0 pt-6">
+              <Image
+                src={breed.image}
+                alt={breed.name}
+                width={200}
+                height={256}
+                className="h-64 w-full rounded-md object-cover object-top"
+              />
+            </CardContent>
+            <CardHeader>
+              <CardDescription className="text-center font-bold">{breed.name}</CardDescription>
+            </CardHeader>
+          </Card>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
